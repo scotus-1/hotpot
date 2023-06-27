adf4960e2380cd5c956ad2ea51ff6223bb76821c
Titus Nguyen <titusstar007@gmail.com>
2023-06-27 13:31:20 -0700

    Add starting script to generate fcsp+-

--- scripts/printside.py
+++ scripts/printside.py
@@ -5,13 +5,17 @@ import toml
 
 mods = [file for file in glob("../mods/*.toml")]
 
+list_output = []
 for filepath in mods:
     toml_file = toml.load(filepath)
     filename = basename(filepath).split(".")[0]
 
-    if toml_file['side'] == "both":
-        print(f"This is a both-sided mod: {toml_file['name']} | {filename} ")
+    if toml_file['side'] == "client":
+        print(f"This is a client-sided mod: {toml_file['name']} | {filename} ")
+        list_output.append(filename)
     # elif toml_file['side'] == "server":
     #     print(f"This is a server-sided mod: {toml_file['name']} | {filename} ")
     # elif toml_file['side'] == "client":
     #     print(f"This is a client-sided mod: {toml_file['name']} | {filename}")
+
+print(list_output)
