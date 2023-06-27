5115a955f263eff49a812ee38b48186314116211
Titus Nguyen <titusstar007@gmail.com>
2023-06-27 14:30:38 -0700

    Tested and removed some mods from fcsp+-, stil dosent work

--- scripts/printside.py
+++ scripts/printside.py
@@ -10,8 +10,8 @@ for filepath in mods:
     toml_file = toml.load(filepath)
     filename = basename(filepath).split(".")[0]
 
-    if toml_file['side'] == "client":
-        print(f"This is a client-sided mod: {toml_file['name']} | {filename} ")
+    if toml_file['side'] == "both":
+        print(f"This is a both-sided mod: {toml_file['name']} | {filename} ")
         list_output.append(filename)
     # elif toml_file['side'] == "server":
     #     print(f"This is a server-sided mod: {toml_file['name']} | {filename} ")
