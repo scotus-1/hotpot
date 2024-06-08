496054a2f56dd4be46bae2db582bfe816c454d8b
Titus Nguyen <titusstar007@gmail.com>
2024-06-08 00:32:24 -0700

    Sided distinctions

--- scripts/printside.py
+++ scripts/printside.py
@@ -1,21 +1,19 @@
 from glob import glob
 from os.path import basename
+import sys
 import toml
 
-
 mods = [file for file in glob("../mods/*.toml")]
+sides = [x.lower() for x in sys.argv]
 
-list_output = []
 for filepath in mods:
     toml_file = toml.load(filepath)
     filename = basename(filepath).split(".")[0]
+    side = toml_file['side']
 
-    if toml_file['side'] == "both":
-        print(f"This is a both-sided mod: {toml_file['name']} | {filename} ")
-        list_output.append(filename)
-    # elif toml_file['side'] == "server":
-    #     print(f"This is a server-sided mod: {toml_file['name']} | {filename} ")
-    # elif toml_file['side'] == "client":
-    #     print(f"This is a client-sided mod: {toml_file['name']} | {filename}")
-
-print(list_output)
+    if side == "both" and side in sides:
+         print(f"This is a both-sided mod: {toml_file['name']} | {filename} ")
+    elif side == "server" and side in sides:
+         print(f"This is a server-sided mod: {toml_file['name']} | {filename} ")
+    elif side == "client" and side in sides:
+        print(f"This is a client-sided mod: {toml_file['name']} | {filename}")
